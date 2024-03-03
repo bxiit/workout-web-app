ab39563082f73d562243dec0f820c5ffb40f8e04
bxit <arturivanov50808@gmail.com>
2024-03-03 19:54:59 +0600

    all customer add and ready

--- src/main/java/spring/code/restapiapp/config/SecurityConfig.java
+++ src/main/java/spring/code/restapiapp/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("api/users", "api/welcome").permitAll()
+                        .requestMatchers("api/users", "api/welcome", "api/all-customers").permitAll()
                         .requestMatchers("api/**").authenticated())
                 .formLogin(Customizer.withDefaults());
 //                .formLogin(login -> login
